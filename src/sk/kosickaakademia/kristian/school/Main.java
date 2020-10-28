e9c8abbce92cd67a33e9bd15ffd24b24427500c8
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-10-28 10:43:53 +0100

    Project school

--- src/sk/kosickaakademia/kristian/school/Main.java
+++ src/sk/kosickaakademia/kristian/school/Main.java
@@ -1,4 +1,8 @@
 package sk.kosickaakademia.kristian.school;
 
 public class Main {
+    public static void main(String[] args) {
+        Grades student1 = new Grades(1,2,3);
+        Student s1 = new Student("Janko", "Hrasko", student1, ClassName.N1);
+    }
 }
